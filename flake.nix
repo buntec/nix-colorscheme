16b1b303ad5e4bc5e13004dfeee0464465427442
Christoph Bunte <christophbunte@gmail.com>
2023-11-13 18:53:34 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "A home-manager module for managing color schemes";
+
+  inputs = {
+
+    tokyonight = {
+      url = "github:folke/tokyonight.nvim";
+      flake = false;
+    };
+
+    nightfox = {
+      url = "github:EdenEast/nightfox.nvim";
+      flake = false;
+    };
+
+    catppuccin-fish = {
+      url = "github:catppuccin/fish";
+      flake = false;
+    };
+
+  };
+
+  outputs = inputs@{ self }: {
+
+    colorscheme = (import ./colorscheme) inputs;
+
+  };
+
+}
